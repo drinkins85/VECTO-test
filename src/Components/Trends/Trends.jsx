4bd7a4f5979db6745ef247fd3004fc268d0420c6
Ахряпов Андрей <AAAkhryapov@sberbank.ru>
2022-03-12 20:47:23 +0300

    wheel scroll

--- src/Components/Trends/Trends.jsx
+++ src/Components/Trends/Trends.jsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import './Trends.css';
 
 const Trends = (props) => {
     const { trends, onClick } = props;
 
+    const ref = useRef();
+
+    const handleWheel = (e) => {
+        if (e.deltaY === 0 || !ref.current) {
+            return;
+        }
+
+        ref.current.scrollTo({
+            left: ref.current.scrollLeft + e.deltaY,
+        });
+    };
+
     return (
         <div className={'Trends'}>
-            {trends && trends.map((trend) => {
-                const { Id, Title, CoverImage } = trend;
+            <div className={'Trends__title'}>Trending Now</div>
+            <div className={'Trends__list'} onWheel={handleWheel} ref={ref}>
+                {trends && trends.map((trend) => {
+                    const { Id, Title, CoverImage } = trend;
 
-                return (
-                    <div className={'Trends__item'} key={Id} onClick={() => { onClick(Id); }}>
-                        {CoverImage && <img
-                            className={'Trends__image'}
-                            alt={Title}
-                            title={Title}
-                            src={`images/${CoverImage}`}
-                        />}
-                    </div>
-                );
-            })}
+                    return (
+                        <div className={'Trends__item'} key={Id} onClick={() => { onClick(Id); }}>
+                            {CoverImage && <img
+                                className={'Trends__image'}
+                                alt={Title}
+                                title={Title}
+                                src={`images/${CoverImage}`}
+                            />}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
